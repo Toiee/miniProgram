b17d6c367af36a1d9a35e34e42ccc0a93ac2fec0
toiee <1137671181@qq.com>
2020-05-21 16:10:44 +0800

    MVVM架构

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,37 @@
-// Pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Mfxx',
+    age: 18,
+    students: [
+      {id: 110, name: 'copy', age: 30},
+      {id: 100, name: 'jems1', age: 34},
+      {id: 125, name: 'why2', age: 35},
+      {id: 145, name: 'codin2', age: 38}
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    // 1. 错误做法
+    // this.data.counter += 1
+    // console.log(this.data.counter)
+    // 2. this.setData()
+    this.setData({
+      counter: this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter-1
+    })
   }
 })
+
+// 编程范式
+// 1. 命令式编程
+// 原生操作dom, jquery 获取监听设置操作....
+// 2. 声明式编程: 框架 vue react 声明式编程
+
+// MVVM 架构让我们从命令式编程转移到声明式编程
+// VM - ViewModel
+// 1. Data Bindings  绑定model层的数据到View
+// 2. DOM Listeners 监听DOM
